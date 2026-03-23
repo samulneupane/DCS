3290107da47d301ea7a22fc67cc46bc74f03a2a8
Chibuikem Ozueh <130606504+Chibuozueh@users.noreply.github.com>
2026-03-23 18:45:07 -0500

    Update Player.java
    move method worked on

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,22 @@
-public class player{
- private Char color;
-  private List <pieces> piecesRemain; //what pieces they got left
-makeMove
+public class makeMove{
+private int[] moveFrom;
+ private int[] moveTo;
+private Piece piece;
+}
+public class Player{
+ private Char Color;
+  private List <piece> piecesRemain; //what pieces they got left
+ 
+ public int[] makeMove(int[] f,int[] t, piece p){
+ boolean valid = false;
+ for (int i=0;i<possibleMoves.size();i++){
+     if (possibleMoves.get(i)[0]==t[0] && possibleMoves.get(i)[1]==t[1]) {
+       valid = true; 
+      }
+    }
+ if (valid){
+  p.setPosition(t[0], t[1]); 
+   }
+ return p.position;
+ }
+}  

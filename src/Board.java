e2606e3f9edaeaa4fe7bea774bc247e52fc262c3
samul <bnv36@txstate.edu>
2026-03-23 18:04:36 -0500

    Added a board array to class WE added a 8*8 2D array called board to Board class

---
+++ src/Board.java
@@ -0,0 +1,10 @@
+public class Board {
+
+    // 8x8 board matrix to hold pieces
+    private Piece[][] board;
+
+    // Constructor
+    public Board() {
+        board = new Piece[8][8]; // initialize empty 8x8 board
+    }
+

537a4796a02d56052e3e1abe119722c124cf3a28
samul <bnv36@txstate.edu>
2026-03-23 18:38:42 -0500

    Added method getPiece() AI Log: Prompted AI to write a method to move pieces and made sure to add errors if there is no piece present to move

--- src/Board.java
+++ src/Board.java
@@ -22,5 +22,29 @@ public class Board {
         int col = pos.getColumn();
         return board[row][col];
     }
+
+    //to move a piece
+    public boolean movePiece(Position from, Position to) {
+    Piece movingPiece = getPiece(from);
+    
+    if (movingPiece == null) {
+        // No piece at source
+        return false;
+    }
+
+    Piece targetPiece = getPiece(to);
+    
+    if (targetPiece != null) {
+        // Capture the piece
+        capturedPieces.add(targetPiece);
+    }
+
+    // Move the piece
+    board[to.getRow()][to.getColumn()] = movingPiece;
+    board[from.getRow()][from.getColumn()] = null;
+    movingPiece.setPosition(to.toString()); // update piece's internal position
+
+    return true;
+}
 }
 

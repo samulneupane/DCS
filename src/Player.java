68b01b960b506507bf14ed6870084341db09f9d7
Chibuikem Ozueh <130606504+Chibuozueh@users.noreply.github.com>
2026-03-23 19:08:09 -0500

    Update Player.java
    more changes to move method

--- src/Player.java
+++ src/Player.java
@@ -3,11 +3,20 @@ private int[] moveFrom;
  private int[] moveTo;
 private Piece piece;
 }
+
 public class Player{
- private Char Color;
-  private List <piece> piecesRemain; //what pieces they got left
+ private string[] Color;
+ private List<Piece> piecesRemain;
+
+  color[0]="B";
+  color[1]="W";
  
- public int[] makeMove(int[] f,int[] t, piece p){
+//modifying pieces remaining 
+ public List<Piece> getPiecesRemain() { return piecesRemain; };
+  public void addPiece(Piece p) { piecesRemain.add(p);}
+  public void removePiece(Piece p) { piecesRemain.remove(p); }
+
+ public int[] makeMove(int[] f,int[] t, Piece p){
  boolean valid = false;
  for (int i=0;i<possibleMoves.size();i++){
      if (possibleMoves.get(i)[0]==t[0] && possibleMoves.get(i)[1]==t[1]) {
@@ -15,8 +24,8 @@ public class Player{
       }
     }
  if (valid){
-  p.setPosition(t[0], t[1]); 
+   movePiece(f,t);
    }
- return p.position;
+ return p.getPosition();
  }
 }  

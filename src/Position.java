dc90af35acd3688ffd7b9d9d85dc1d20e66ab414
Chibuikem Ozueh <130606504+Chibuozueh@users.noreply.github.com>
2026-03-23 19:50:28 -0500

    Update Position.java
    AI log- generated getters and setter to save time.

--- src/Position.java
+++ src/Position.java
@@ -1,9 +1,6 @@
 public class Position {
     private int row;
     private int col;
-    public class Position {
-    private int row;
-    private int col;
 
     public Position(int row, int col) {
         this.row = row;
@@ -12,7 +9,6 @@ public class Position {
 
     public int getRow() { return row; }
     public int getCol() { return col; }
-
     
     public void setRow(int row) { this.row = row; }
     public void setCol(int col) { this.col = col; }

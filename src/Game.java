e66ed0ab5713437ee0c8b5534e7bc1a225f119c9
Chibuikem Ozueh <130606504+Chibuozueh@users.noreply.github.com>
2026-03-23 20:32:04 -0500

    Update Game.java
    AI log- trouble shooting naming and syntax

--- src/Game.java
+++ src/Game.java
@@ -1,16 +1,24 @@
 public class Game{
   public Board board;
-  private player white;
-  private player black;
+  private Player white;
+  private Player black;
   public int currentTurn; 
 
    public Game() {
      
-   Color white = new Color('W', 1);
-  Color black = new Color('B', 2);
+   Color whiteColor = new Color('W', 1);
+   Color blackColor = new Color('B', 2);
 
-   white = new Player(white);
-   black = new Player(black);
-   currentTurn= 1;
+   white = new Player(whiteColor);
+   black = new Player(blackColor);
+   currentTurn = 1;
+
 }
+  public void start() {
+        board = new Board();
+        board.initialize();
+    }
+  public string end(){
+    
+  }
 }

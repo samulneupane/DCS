d68bfcfbd6a4def3b5c8ea734f9d3ab5cb4e3730
“Dariengit config --global user.email “lfn7@txstate.edu <lfn7@txstatw.edu>
2026-03-23 20:05:09 -0500

    Add queen movement in all straight and diagonal directions

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -6,21 +6,20 @@ import enums.Color;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Knight extends Piece {
+public class King extends Piece {
 
-    public Knight(Color color, Position position) {
+    public King(Color color, Position position) {
         super(color, position);
     }
 
     @Override
     public List<Position> possibleMoves(Board board) {
         List<Position> moves = new ArrayList<>();
-        
+
         int[][] offsets = {
-            {-2, -1}, {-2, 1},
-            {-1, -2}, {-1, 2},
-            {1, -2}, {1, 2},
-            {2, -1}, {2, 1}
+            {-1, -1}, {-1, 0}, {-1, 1},
+            {0, -1},           {0, 1},
+            {1, -1},  {1, 0},  {1, 1}
         };
 
         int row = position.getRow();
@@ -28,9 +27,19 @@ public class Knight extends Piece {
 
         for (int[] offset : offsets) {
             Position p = new Position(row + offset[0], col + offset[1]);
+
             if (board.isInsideBoard(p)) {
                 if (board.getPiece(p) == null || board.getPiece(p).getColor() != color) {
                     moves.add(p);
                 }
             }
         }
+
+        return moves;
+    }
+
+    @Override
+    public String getSymbol() {
+        return color == Color.WHITE ? "wk" : "bk";
+    }
+}
---
+++ src/pieces/Queen.java
@@ -0,0 +1,40 @@
+package pieces;
+
+import board.Board;
+import board.Position;
+import enums.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Queen extends Piece {
+
+    public Queen(Color color, Position position) {
+        super(color, position);
+    }
+
+    @Override
+    public List<Position> possibleMoves(Board board) {
+        List<Position> moves = new ArrayList<>();
+
+        int row = position.getRow();
+        int col = position.getCol();
+        int[][] directions = {
+            {-1, 0}, {1, 0}, {0, -1}, {0, 1},
+            {-1, -1}, {-1, 1}, {1, -1}, {1, 1}
+        };
+
+        for (int[] dir : directions) {
+            int r = row + dir[0];
+            int c = col + dir[1];
+
+            while (r >= 0 && r < 8 && c >= 0 && c < 8) {
+                Position p = new Position(r, c);
+
+                if (board.getPiece(p) == null) {
+                    moves.add(p);
+                } else {
+                    if (board.getPiece(p).getColor() != color) {
+                        moves.add(p);
+                    }
+                    break;
+                }

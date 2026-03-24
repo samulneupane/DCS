024f751fd0e62988a3bcc8ed23db7f1979f1f7f9
samul <bnv36@txstate.edu>
2026-03-23 20:02:44 -0500

    Created Position class initialized it with row and coloumn

---
+++ src/Position.java
@@ -0,0 +1,10 @@
+public class Position {
+    private int row;
+    private int column;
+
+    //constructor
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }    
+}

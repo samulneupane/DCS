2f97eb0e935ce0b08f68562845cf774aef01d0c1
Chibuikem Ozueh <130606504+Chibuozueh@users.noreply.github.com>
2026-03-23 20:16:36 -0500

    Update Game.java
    AI log- trouble shooting and confirming structure + syntax

--- src/Game.java
+++ src/Game.java
@@ -3,4 +3,14 @@ public class Game{
   private player white;
   private player black;
   public int currentTurn; 
+
+   public Game() {
+     
+   Color white = new Color('W', 1);
+  Color black = new Color('B', 2);
+
+   white = new Player(white);
+   black = new Player(black);
+   currentTurn= 1;
+}
 }

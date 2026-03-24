55ba8a0dd9fd35659a71276de1ddb275fa7e9104
“Dariengit config --global user.email “lfn7@txstate.edu <lfn7@txstatw.edu>
2026-03-23 19:51:53 -0500

    Implement knight move validation and symbol logic

--- src/pieces/Knight.java
+++ src/pieces/Knight.java
@@ -21,6 +21,21 @@ public class Knight extends Piece {
             {1, -2}, {1, 2},
             {2, -1}, {2, 1}
         };
-}
+        int row = position.getRow();
+        int col = position.getCol();
 
+        for (int[] offset : offsets) {
+            Position p = new Position(row + offset[0], col + offset[1]);
+            if (board.isInsideBoard(p)) {
+                if (board.getPiece(p) == null || board.getPiece(p).getColor() != color) {
+                    moves.add(p);
+                }
+            }
+        }
+        return moves;
+        @Override
+    public String getSymbol() {
+        return color == Color.WHITE ? "wn" : "bn";
+    }
+    }
 }

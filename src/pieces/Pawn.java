62da6eeaa4e1aabb2d4c9b0f71b9defdd1d3a8e3
samul <bnv36@txstate.edu>
2026-03-25 12:48:24 -0500

    Solving errors in Pawn class AI prompt: point out errors in the following code

--- src/pieces/Pawn.java
+++ src/pieces/Pawn.java
@@ -1,31 +1,37 @@
 package pieces;
 
-import board.Board;
-import board.Position;
-import enums.Color;
+import src.Board;
+import src.Position;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Pawn extends Piece {
-    public Pawn(Color color, Position position) {
-        super(color, position); // Show the position the pawn is on and the color it is
-    }
-    public List<Position> possibleMoves(Board board) {
-        List <Postion> moves = new ArrayList<>();
 
-        int direction = (color == color.WHITE) ? -1 : 1;
+    public Pawn(String color, Position position) {
+        super(color, position);
+    }
+
+    @Override
+    public List<Position> possibleMoves(Board board) {
+        List<Position> moves = new ArrayList<>();
+
+        int direction = color.equals("white") ? -1 : 1; // white moves up, black moves down
         int row = position.getRow();
         int col = position.getColumn();
 
+        // One step forward
         Position oneStep = new Position(row + direction, col);
-          if (board.isInsideBoard(oneStep) && board.getPiece(oneStep) == null) {
+        if (board.isInsideBoard(oneStep) && board.getPiece(oneStep) == null) {
             moves.add(oneStep);
+        }
+
+        // TODO: add two-step initial move and capture diagonals if needed
+
+        return moves;
     }
-    return moves;
-    }
+
     @Override
     public String getSymbol() {
-        return color == Color.WHITE ? "wp" : "bp";
+        return color.equals("white") ? "wP" : "bP";
     }
- }    
-
+}

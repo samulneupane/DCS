40733d3c07db511eaf3db9e68b8659ae74f250c0
samul <bnv36@txstate.edu>
2026-03-27 17:28:22 -0500

    fixing errors in Bishop

--- src/pieces/Bishop.java
+++ src/pieces/Bishop.java
@@ -1,7 +1,7 @@
 package pieces;
 
-import src.Position;
-import src.Board;
+import utils.Board;
+import utils.Position;
 import java.util.ArrayList;
 import java.util.List;
 import java.awt.Color;
@@ -9,73 +9,66 @@ import java.awt.Color;
 
 public class Bishop extends Piece {
 
-    public Bishop(String color, Position position) {
+    public Bishop(Color color, Position position) {
         super(color, position);
     }
 
     @Override
     public List<Position> possibleMoves(Board board) {
         List<Position> moves = new ArrayList<>();
+
         int row = position.getRow();
         int col = position.getColumn();
 
-        // Top-left diagonal
         for (int r = row - 1, c = col - 1; r >= 0 && c >= 0; r--, c--) {
             Position p = new Position(r, c);
             if (board.getPiece(p) == null) {
                 moves.add(p);
             } else {
-                if (!board.getPiece(p).getColor().equals(color)) {
+                if (board.getPiece(p).getColor() != color) {
                     moves.add(p);
                 }
                 break;
             }
         }
-
-        // Top-right diagonal
         for (int r = row - 1, c = col + 1; r >= 0 && c < 8; r--, c++) {
             Position p = new Position(r, c);
             if (board.getPiece(p) == null) {
                 moves.add(p);
             } else {
-                if (!board.getPiece(p).getColor().equals(color)) {
+                if (board.getPiece(p).getColor() != color) {
                     moves.add(p);
                 }
                 break;
             }
         }
-
-        // Bottom-left diagonal
         for (int r = row + 1, c = col - 1; r < 8 && c >= 0; r++, c--) {
             Position p = new Position(r, c);
             if (board.getPiece(p) == null) {
                 moves.add(p);
             } else {
-                if (!board.getPiece(p).getColor().equals(color)) {
+                if (board.getPiece(p).getColor() != color) {
                     moves.add(p);
                 }
                 break;
             }
         }
-
-        // Bottom-right diagonal
         for (int r = row + 1, c = col + 1; r < 8 && c < 8; r++, c++) {
             Position p = new Position(r, c);
             if (board.getPiece(p) == null) {
                 moves.add(p);
             } else {
-                if (!board.getPiece(p).getColor().equals(color)) {
+                if (board.getPiece(p).getColor() != color) {
                     moves.add(p);
                 }
                 break;
             }
         }
-
         return moves;
     }
 
     @Override
     public String getSymbol() {
-        return color.equals("white") ? "wB" : "bB";
+        return color == Color.WHITE ? "wb" : "bb";
     }
 }

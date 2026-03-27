ba76f98fa2c9e3148c7077955bdab8d85b0b6df4
samul <bnv36@txstate.edu>
2026-03-27 17:37:31 -0500

    fixing errors in Rook

--- src/pieces/Rook.java
+++ src/pieces/Rook.java
@@ -1,69 +1,64 @@
 package pieces;
 
-import src.Board;
-import src.Position;
+import utils.Board;
+import utils.Position;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.awt.Color;
 
 public class Rook extends Piece {
 
-    public Rook(String color, Position position) {
+    public Rook(Color color, Position position) {
         super(color, position);
     }
 
     @Override
     public List<Position> possibleMoves(Board board) {
         List<Position> moves = new ArrayList<>();
+
         int row = position.getRow();
         int col = position.getColumn();
-
-        // Vertical up
-        for (int r = row - 1; r >= 0; r--) {
+for (int r = row - 1; r >= 0; r--) {
             Position p = new Position(r, col);
             if (board.getPiece(p) == null) {
                 moves.add(p);
             } else {
-                if (!board.getPiece(p).getColor().equals(this.color)) {
+                if (board.getPiece(p).getColor() != color) {
                     moves.add(p);
                 }
                 break;
             }
         }
-
-        // Vertical down
         for (int r = row + 1; r < 8; r++) {
             Position p = new Position(r, col);
             if (board.getPiece(p) == null) {
                 moves.add(p);
             } else {
-                if (!board.getPiece(p).getColor().equals(this.color)) {
+                if (board.getPiece(p).getColor() != color) {
                     moves.add(p);
                 }
                 break;
             }
         }
 
-        // Horizontal left
         for (int c = col - 1; c >= 0; c--) {
             Position p = new Position(row, c);
             if (board.getPiece(p) == null) {
                 moves.add(p);
             } else {
-                if (!board.getPiece(p).getColor().equals(this.color)) {
+                if (board.getPiece(p).getColor() != color) {
                     moves.add(p);
                 }
                 break;
             }
         }
-
-        // Horizontal right
         for (int c = col + 1; c < 8; c++) {
             Position p = new Position(row, c);
             if (board.getPiece(p) == null) {
                 moves.add(p);
             } else {
-                if (!board.getPiece(p).getColor().equals(this.color)) {
+                if (board.getPiece(p).getColor() != color) {
                     moves.add(p);
                 }
                 break;
@@ -72,9 +67,8 @@ public class Rook extends Piece {
 
         return moves;
     }
-
     @Override
     public String getSymbol() {
-        return color.equals("white") ? "wR" : "bR";
+        return color == Color.WHITE ? "wr" : "br";
     }
 }

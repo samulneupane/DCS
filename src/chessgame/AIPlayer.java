26f2ced83355dacd12622f77f2dbda56e4692300
samul <bnv36@txstate.edu>
2026-04-29 14:09:05 -0500

    Imported all the necessary classes

--- src/chessgame/AIPlayer.java
+++ src/chessgame/AIPlayer.java
@@ -1,1 +1,9 @@
 package chessgame;
+
+import pieces.Piece;
+import utils.Board;
+import utils.Position;
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;

4f96a6ac6432c1510ab853a1699657330b75bd81
samul <bnv36@txstate.edu>
2026-04-29 14:09:40 -0500

    Add AIPlayer class with color and random fields

--- src/chessgame/AIPlayer.java
+++ src/chessgame/AIPlayer.java
@@ -7,3 +7,14 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+
+public class AIPlayer {
+
+    private Color color;
+    private Random random;
+
+    public AIPlayer(Color color) {
+        this.color = color;
+        this.random = new Random();
+    }
+}
